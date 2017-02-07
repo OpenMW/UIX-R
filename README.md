38063e2412ff9712dd300c485d6a0b7b5bb461da
Bret Curtis <psi29a@gmail.com>
2017-02-07 17:19:44 +0100

    it makes releases and we have updated assets

--- README.md
+++ README.md
@@ -1,8 +1,43 @@
-# Ultima IX: Redemption for Morrowind and OpenMW
+# Ultima IX: Redemption for Morrowind and OpenMW, Libre Edition
+
+This is an attempt to make a Free and Open Source release of the assets used to create 
+Ultima IX: Redemption. With these releases, you are free to use the models, meshes and animations
+to create, remix and build your own simulations, games or worlds.
+
+## Methodology and Release Guidelines
+
+We are curating the original UIX:R release and validating that none of the assets used contain 
+Bethesda's intellectual property (IP) and copyrights. We then search for the original creator of
+the work and ask their permission to apply or otherwise re-license to a Creative Common License
+of their choice.
+
+## Available CC licenses
+
+Currently 100% of the assets available fall under the CC0 license, otherwise known as the 
+"Public Domain" license for jurisdictions that don't honour the concept of "Public Domain".
+
+As more content creators are contacted, they can pick a CC license that bests suites them.
+
+## People you should thank:
+
+Alain "Spyder" Brunotte, released over a 1000 assets
+
+## People we are looking to make contact with:
+
+If you have any information leading to the following people, please create a ticket and we can
+talk further!
+
+barabus
+Brad "Operadragon" Venable
+Dustin Naegel
+Jeremy McGuinn - Knights of Tamriel
+Matti Paalanen
+Star Boi
+Steven "Chlorthos" McCrary
+
+## Original UIX:R
 
 Morrowind Mod UIX – landscape, story and music release – history of the mod and the team.:
 http://www.titansofether.com/uix/
-
-##  
 
 
--- make_release.py
+++ make_release.py
@@ -6,7 +6,7 @@ import os
 import sys
 import tarfile
 
-RELEASE_PATH = "uixr-assets-cc0-1.0.tar.gz"
+RELEASE_PATH = "uixr-assets-cc0-1.0.tar"
 ASSET_PATH = "../../../Private"
 
 
@@ -22,12 +22,12 @@ def get_filename(string):
 
 with open("Manifests/UIXR.manifest") as f:
     uixr_data = csv.DictReader(f)
-    tar_ball = tarfile.open(RELEASE_PATH, "w:gz")
+    tar_ball = tarfile.open(RELEASE_PATH, "w:")
     spinner = spinning_cursor()
     additional_assets = []
     print("Gathering assets: ")
     for row in uixr_data:
-        if row.get('license').upper() == 'CC0':
+        if row.get('license').lower() == 'cc0':
             sys.stdout.write("\033[K")
             sys.stdout.write(next(spinner))
             sys.stdout.write(" [{0}]".format(row.get('asset')))
@@ -48,22 +48,29 @@ with open("Manifests/UIXR.manifest") as f:
     if '' in additional_assets:
         additional_assets.remove('')
 
+    print("Gathering sub-assets: ")
     # iterate through all sub assets
     for nif_asset in additional_assets:
-        for asset_path in glob.iglob(os.path.join(ASSET_PATH, "UIX", "**", nif_asset), recursive=True):
+        found = False
+        for asset_path in glob.iglob(os.path.join(ASSET_PATH, "UIX", "**", nif_asset),
+                                     recursive=True):
+            found = True
             relative_asset_path = os.path.relpath(os.path.realpath(asset_path), ASSET_PATH)
             if relative_asset_path in tar_ball.getnames():
-                continue  # file already exists, skip
+                break  # file already exists, skip
             sys.stdout.write("\033[K")
             sys.stdout.write(next(spinner))
             sys.stdout.write(" [{0}]".format(relative_asset_path))
             sys.stdout.flush()
             sys.stdout.write('\r\b')
-            if relative_asset_path == '.':
-                import pdb; pdb.set_trace()
-                continue  # skip this before we pull in everything
-
+            f.seek(0)   # reset to beginning of csv file
+            for row in uixr_data:
+                if row.get('asset').lower() == relative_asset_path.lower():
+                    if row.get('license').lower() == 'cc0':
+                        break  # good to go, break
+                    else:
+                        print("WARNING: Non-CC0 license asset -> {0}".format(relative_asset_path))
             tar_ball.add(asset_path, relative_asset_path)
-            # print("Additional asset: {0}".format(relative_asset_path))
-
+        if not found:
+            print("WARNING: sub-asset not found -> {0}".format(nif_asset))
     tar_ball.close()
