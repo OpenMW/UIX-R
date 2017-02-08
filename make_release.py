2f71cd225e3c287cf087d620ff617c6e5737ef50
Bret Curtis <psi29a@gmail.com>
2017-02-08 17:20:19 +0100

    move nif.assets, update manifest with additional cc0 assets from spyder and make_release too finalization

--- make_release.py
+++ make_release.py
@@ -20,10 +20,17 @@ def get_filename(string):
     return os.path.basename(string.replace("\\", "/"))
 
 
+def insensitive_glob(pattern, recursive):
+    def either(c):
+        return '[%s%s]' % (c.lower(), c.upper()) if c.isalpha() else c
+    return glob.iglob(''.join(map(either, pattern)), recursive=recursive)
+
+
 with open("Manifests/UIXR.manifest") as f:
     uixr_data = csv.DictReader(f)
     tar_ball = tarfile.open(RELEASE_PATH, "w:")
     spinner = spinning_cursor()
+    manifest = []
     additional_assets = []
     print("Gathering assets: ")
     for row in uixr_data:
@@ -34,6 +41,11 @@ with open("Manifests/UIXR.manifest") as f:
             sys.stdout.flush()
             sys.stdout.write('\r\b')
             file_path = os.path.join(ASSET_PATH, row.get('asset'))
+            if not os.path.exists(file_path):
+                print("WARNING: asset not found -> {0}".format(row.get('asset')))
+                continue
+
+            manifest.append(row.get('asset'))
             tar_ball.add(file_path, row.get('asset'))
 
             # check that we have all related assets
@@ -42,18 +54,25 @@ with open("Manifests/UIXR.manifest") as f:
                 for asset in asset_string.split(', '):
                     additional_assets.append(get_filename(asset))
 
+    # remove duplicates
     additional_assets = set(additional_assets)
 
     # remove blank entry in set
     if '' in additional_assets:
         additional_assets.remove('')
 
+    # remove files listed in manifest, we already have them
+    for asset in manifest:
+        filename = os.path.basename(asset)
+        if filename in additional_assets:
+            additional_assets.remove(filename)
+
     print("Gathering sub-assets: ")
     # iterate through all sub assets
     for nif_asset in additional_assets:
         found = False
-        for asset_path in glob.iglob(os.path.join(ASSET_PATH, "UIX", "**", nif_asset),
-                                     recursive=True):
+        glob_path = os.path.join(ASSET_PATH, "UIX", "**", nif_asset)
+        for asset_path in insensitive_glob(glob_path, True):
             found = True
             relative_asset_path = os.path.relpath(os.path.realpath(asset_path), ASSET_PATH)
             if relative_asset_path in tar_ball.getnames():
